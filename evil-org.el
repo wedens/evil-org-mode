e57a0f143251913b425160294a38e5fb0cbbb5f3
Henrik Lissner <henrik@lissner.net>
2019-10-23 17:20:19 -0400

    Fix :wq in org-src edit buffers

--- evil-org.el
+++ evil-org.el
@@ -796,6 +796,12 @@ Optional argument THEME list of themes. See evil-org-keytheme for a list of valu
      (assq 'evil-org-mode minor-mode-map-alist)
      evil-org-mode-map)))
 
+(defun evil-org-edit-src-exit (command)
+  "Save then `evil-edit-src-exit'."
+  (interactive)
+  (call-interactively #'evil-write)
+  (call-interactively command))
+
 ;;; vim-like confirm/abort for capture and src
 ;;; Taken from mwillsey (Max Willsey) on https://github.com/syl20bnr/spacemacs/pull/7400
 (with-eval-after-load 'org-capture
@@ -804,8 +810,8 @@ Optional argument THEME list of themes. See evil-org-keytheme for a list of valu
   (define-key org-capture-mode-map [remap evil-quit]                    'org-capture-kill))
 
 (with-eval-after-load 'org-src
-  (define-key org-src-mode-map [remap evil-save-and-close]          'org-edit-src-exit)
-  (define-key org-src-mode-map [remap evil-save-modified-and-close] 'org-edit-src-exit)
+  (define-key org-src-mode-map [remap evil-save-and-close]          'evil-org-edit-src-exit)
+  (define-key org-src-mode-map [remap evil-save-modified-and-close] 'evil-org-edit-src-exit)
   (define-key org-src-mode-map [remap evil-quit]                    'org-edit-src-abort))
 
 (with-eval-after-load 'org-table
